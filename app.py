973966935deee46ea44c407821b3615367d1fe8a
Tommy Heyde Olsen <tommy@heyde-olsen.dk>
2017-06-11 16:16:48 +0200

    Added /time

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+import time
 
 @app.route('/')
 def hello_world():
     return 'Hello world, from Docker with Flask'
-    if __name__ == '__main__':
-        app.run(debug=True,host='0.0.0.0')
+
+@app.route('/time')
+def time():
+    return time.time()
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
